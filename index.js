3cbea08a0f5b1a71f3df872e31b770dc1661d0e8
Guillermo <gonzalezdecastro.guillermo@gmail.com>
2016-02-06 19:28:39 +0100

    initial

---
+++ index.js
@@ -0,0 +1,47 @@
+
+/**
+ *
+ * @title simple_progression
+ * @artist Guilletr0n
+ * @license mit
+ *
+ */
+
+var transpose = 0;
+
+var bassline = [ 3,5,7,8,10,12,14,15];
+
+export function dsp(t){
+  
+  var melody = note(bassline[Math.floor(t * 2) % bassline.length], 0);
+  
+  var tone =   sin(melody, t);
+
+  // mixer
+  return 0.4 * (
+    clip(0.1, tone)
+  );
+}
+
+
+function sin(x, t){
+  return Math.sin(2 * Math.PI * t * x);
+}
+
+function clip(n, x){
+  return x > n
+    ? n
+    : x < -n
+    ? -n
+    : x
+  ;
+}
+
+// gets note `n` frequency of `octave`
+function note(n, octave){
+  return Math.pow(
+    2,
+    (n + transpose - 33 + (12 * (octave || 0))) / 12
+  ) * 440; // A4 tune
+}
+
